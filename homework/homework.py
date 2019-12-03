5a8246e2c9b4504248e7e018201d5491927d0572
MikiKru <michal_kruczkowski@o2.pl>
2019-12-03 10:38:30 +0100

    warcaby v2

--- homework/homework.py
+++ homework/homework.py
@@ -1,22 +1,51 @@
 # CTRL + ALT + L -> auto-formatowanie
+
 class Warcaby:
-    # 
+    # pole klasowe -> słownik zawierający 8 słowników
     warcaby = {
-        8: {'a': "Empty", 'b': "WHITE", 'c': "Empty", 'd': "WHITE", 'e': "Empty", 'f': "WHITE", 'g': "Empty",
-            'h': "WHITE"},
-        7: {'a': "WHITE", 'b': "Empty", 'c': "WHITE", 'd': "Empty", 'e': "WHITE", 'f': "Empty", 'g': "WHITE",
-            'h': "Empty"},
-        6: {'a': "Empty", 'b': "White", 'c': "Empty", 'd': "White", 'e': "Empty", 'f': "White", 'g': "Empty",
-            'h': "White"},
-        5: {'a': "Empty", 'b': "Empty", 'c': "Empty", 'd': "Empty", 'e': "Empty", 'f': "Empty", 'g': "Empty",
-            'h': "Empty"},
-        4: {'a': "Empty", 'b': "Empty", 'c': "Empty", 'd': "Empty", 'e': "Empty", 'f': "Empty", 'g': "Empty",
-            'h': "Empty"},
-        3: {'a': "BLACK", 'b': "Empty", 'c': "BLACK", 'd': "Empty", 'e': "BLACK", 'f': "Empty", 'g': "BLACK",
-            'h': "Empty"},
-        2: {'a': "Empty", 'b': "BLACK", 'c': "Empty", 'd': "BLACK", 'e': "Empty", 'f': "BLACK", 'g': "Empty",
-            'h': "BLACK"},
-        1: {'a': "BLACK", 'b': "Empty", 'c': "BLACK", 'd': "Empty", 'e': "BLACK", 'f': "Empty", 'g': "BLACK",
-            'h': "Empty"}
+        8: {1: "_", 2: "O", 3: "_", 4: "O", 5: "_", 6: "O", 7: "_",8: "O"},
+        7: {1: "O", 2: "_", 3: "O", 4: "_", 5: "O", 6: "_", 7: "O",8: "_"},
+        6: {1: "_", 2: "O", 3: "_", 4: "O", 5: "_", 6: "O", 7: "_",8: "O"},
+        5: {1: "_", 2: "_", 3: "_", 4: "_", 5: "_", 6: "_", 7: "_",8: "_"},
+        4: {1: "_", 2: "_", 3: "_", 4: "_", 5: "_", 6: "_", 7: "_",8: "_"},
+        3: {1: "X", 2: "_", 3: "X", 4: "_", 5: "X", 6: "_", 7: "X",8: "_"},
+        2: {1: "_", 2: "X", 3: "_", 4: "X", 5: "_", 6: "X", 7: "_",8: "X"},
+        1: {1: "X", 2: "_", 3: "X", 4: "_", 5: "X", 6: "_", 7: "X",8: "_"}
     }
 
+    # metoda klasowa wypisująca szachownicę
+    def printBoard(self):
+        # iteracja po sekwencji wierszy
+        print("  | %1s | %1s | %1s | %1s | %1s | %1s | %1s | %1s |  " % ("A", "B", "C", "D", "E", "F", "G", "H"))
+        for row in self.warcaby.keys():
+            # iteracja po polach w wierszu
+            print("%1d " % (row), end="")
+            for key in self.warcaby[row].keys():
+                print("| %1s " % (self.warcaby[row][key]), end="")
+            print("| %1d \n" % (row), end="")
+        print("  | %1s | %1s | %1s | %1s | %1s | %1s | %1s | %1s |  " % ("A", "B", "C", "D", "E", "F", "G", "H"))
+
+    def getPoint(self):
+        print(self.warcaby[3][3])
+
+    def checkMoveFromTo(self, rowStart, columnStart, rowStop, columnStop, type):
+        # sprawdzam czy punkty znajdują się w kluczach naszych słowników
+        if(rowStart in self.warcaby.keys() and columnStart in self.warcaby[rowStart].keys()):
+            # sprawdzenie poprawności ruchu pionka X
+            if(type == "X" and rowStop == (rowStart + 1)
+                    and (columnStop == (columnStart + 1) or (columnStop == (columnStart - 1)))
+                    and columnStop >= 1 and columnStop <= 8 and rowStop >= 1 and rowStop <=8):
+                print("ruch poprawny dla pionka x")
+            # sprawdzenie poprawności ruchu pionka O
+            elif(type == "O" and rowStop == (rowStart - 1)
+                    and (columnStop == (columnStart + 1) or (columnStop == (columnStart - 1)))
+                    and columnStop >= 1 and columnStop <= 8 and rowStop >= 1 and rowStop <= 8):
+                print("ruch poprawny dla pionka o")
+            else:
+                print("Błędny ruch")
+        else:
+            print("błędny adres pionka")
+w1 = Warcaby()
+w1.printBoard()
+w1.getPoint()
+w1.checkMoveFromTo(3,1,4,0,"X")
