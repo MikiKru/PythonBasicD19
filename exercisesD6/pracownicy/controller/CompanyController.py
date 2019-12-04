1482f1f8a3dcca841dacab70a9c730f89ef90efe
MikiKru <michal_kruczkowski@o2.pl>
2019-12-04 11:57:10 +0100

    polimorfizm v7

---
+++ exercisesD6/pracownicy/controller/CompanyController.py
@@ -0,0 +1,15 @@
+class CompanyController:
+    employees = []
+    #1. dodawanie pracownika lub praktykanta z unikatowym loginem
+
+    #2. wyświetlenie wszystkich pracowników i praktykantów posortowanych po pencji DESC
+
+    # przypisanie nagrody do pracownika lub praktykanta
+
+    # zmiana pensji tylko dla pracownika
+
+    # usuwanie pracownika lub praktykanta z listy
+
+    # wyświetlenie tylko kierowników
+
+    # wyświetlenie praktykantów
