73a06a37cebce0ce3f519292e677e5a23c45794b
MikiKru <michal_kruczkowski@o2.pl>
2019-12-04 11:39:08 +0100

    polimorfizm v5

--- exercisesD6/pracownicy/model/Trainee.py
+++ exercisesD6/pracownicy/model/Trainee.py
@@ -1,17 +1,18 @@
+import hashlib
 class Trainee:
     def __init__(self, login, password):
         self.login = login
-        self.password = password
+        self.password = hashlib.md5(("salt:XYZ"+password).encode('utf-8')).digest()
         self.possition = "PRAKTYKANT"
         self.salary = 0
     def assignPrise(self, amount):
         self.salary = amount
     def __str__(self):
-        return "| %15s | %15s | %15s | %13.2fzł |" % \
-        (self.login, 10 * "*", self.possition, self.salary)
+        return "| %15s | %55s | %15s | %13.2fzł |" % \
+        (self.login, self.password, self.possition, self.salary)
 
-#test
-t = Trainee("MichałK","qwe123")
-t.assignPrise(-1000)
-print(t)
+# #test
+# t = Trainee("MichałK","qwe123")
+# t.assignPrise(-1000)
+# print(t)
 
