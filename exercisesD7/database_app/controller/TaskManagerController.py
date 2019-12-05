e5e1377e92186076a617e1eca920e48d5b66818b
MikiKru <michal_kruczkowski@o2.pl>
2019-12-05 13:28:16 +0100

    database v8

--- exercisesD7/database_app/controller/TaskManagerController.py
+++ exercisesD7/database_app/controller/TaskManagerController.py
@@ -1,7 +1,10 @@
 import exercisesD7.sql.database_credentials as secret
+from exercisesD7.database_app.model.User import User
 import pymysql
 
+
 class TaskManagerController:
+    users = []      # koresponduje z rekordami w tabelce user
     def __init__(self):
         self.connection = pymysql.connect(
             host=secret.host,
@@ -11,3 +14,21 @@ class TaskManagerController:
             charset='utf8'
         )
         print("...CONNECTED...")
+        self.cursor = self.connection.cursor()
+
+    def inserdUser(self, email, password, name, lastname, gender):
+        u = User(email,password,name,lastname,gender)
+        # wykoananie polecenia SQL -> nie zwraca waniku
+        self.cursor.execute("INSERT INTO user VALUES (default, %s, %s, %s, %s, %s)",
+                            u.email, u.password, u.name, u.lastname, u.gender)
+        print("DODANO",u.email)
+    def selectUsers(self):
+        # wykoanie zapytania SQL -> zwraca wynik
+        self.cursor.execute("SELECT * FROM user")
+        result = self.cursor.fetchall()     # pobieramy wszystkie wyniki
+        for row in result:
+            u = User(row[1], row[2], row[3], row[4], row[5])
+            print(u)
+
+
+
