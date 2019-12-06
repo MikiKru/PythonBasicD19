1230f856a4536ea5263b474f10a102b63a945103
MikiKru <michal_kruczkowski@o2.pl>
2019-12-06 12:29:23 +0100

    web scrapping v5

--- exercisesD8/stooq/controller/StooqScrappingController.py
+++ exercisesD8/stooq/controller/StooqScrappingController.py
@@ -15,15 +15,33 @@ stooq_html = BeautifulSoup(stooq_page.content, 'html.parser')
 
 import re
 date_pattern = re.compile(".{3}, [0-9]{1,2} .{3}, [0-9]{1,2}:[0-9]{1,2}")
-title_pattern = re.compile()
+title_pattern = re.compile("^.{20,}$")
 
 rows = stooq_html.find_all("tr")
+result = [[],[]]
+
 print("Rekordy")
 for index, row in enumerate(rows):
-    # filtrowanie daty
-    date = str(row.find("td", {"id": "f13"})).replace('<td id="f13" nowrap="">', "").replace("</td>", "")
-    # szukanie tylko dat zgodnych z regexp
-    # if(re.search(date_pattern, date) is not None):
-    #     print("Data:", date)
-    if(len(str(row.a).split(">")) > 1):
-        print("Title:", str(row.a).split(">")[1].replace("</a",""))
+    if(index > 4):
+        # filtrowanie daty
+        date = str(row.find("td", {"id": "f13"})).replace('<td id="f13" nowrap="">', "").replace("</td>", "")
+        if (len(str(row.a).split(">")) > 1):
+            title = str(row.a).split(">")[1].replace("</a","")
+            # szukanie tylko dat zgodnych z regexp
+            if(re.search(date_pattern, date) is not None):
+                # print("Data:", date)
+                result[1].append(date)
+            # szukamt tylko tytułów zgodnych z regexp
+            if(re.search(title_pattern,title) is not None and title[0:4] != "<img"):
+                # print("Title:", title)
+                result[0].append(title)
+            if(title[0:4] == "<img"):
+                title = "obrazek"
+                result[0].append(title)
+                # print("obrazek - tytuł do pobrania")
+
+
+for i, title in enumerate(result[0]):
+    if i > 0:
+        print(title)
+        print(result[1][i])
