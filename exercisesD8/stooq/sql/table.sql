fe3239d725727aea08947984f03806bd3e8981cc
MikiKru <michal_kruczkowski@o2.pl>
2019-12-06 16:31:23 +0100

    web scrapping v24

--- exercisesD8/stooq/sql/table.sql
+++ exercisesD8/stooq/sql/table.sql
@@ -1,9 +1,9 @@
 use python_db;
 
 create table stooq(
-	stooq_id int primary key auto_increment,
+	  stooq_id int primary key auto_increment,
     title text not null,
-	content longtext not null,
-    date_added varchar(55) not null,
+	  content longtext not null,
+    date_added datetime not null,
     status bool default 0
 );
